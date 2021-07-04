81ff29cd9da686b5507ccd5acd0c4c97c80ad394
Ahmed Mire <afmire877@gmail.com>
2021-07-04 11:01:24 +0100

    Added Server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
 export * from './lib/async';
 export * from './lib/number';
+import http from 'http';
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+import proxyRoute from './routes/proxy';
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+// Express set up
+const app = express();
+app.set('port', PORT);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', proxyRoute);
+
+const server = http.createServer(app);
+server.listen(PORT);
+server.on('listening', () => {
+  console.log('Listening on port', PORT);
+});
