f0602ed0954ab05f99304152300ad70b649dbcf1
Ahmed Mire <afmire877@gmail.com>
2021-07-14 21:38:58 +0100

    Added Cors fix and env config

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,19 @@ import http from 'http';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
-
 import proxyRoute from './routes/proxy';
 const PORT = process.env.PORT || 5000;
-
-dotenv.config();
 
 // Express set up
 const app = express();
 app.set('port', PORT);
 app.use(cors());
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
