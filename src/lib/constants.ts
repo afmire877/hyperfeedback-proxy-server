71885ab40749066af6402db5f376f28a34a35fff
Ahmed <afmire877@gmail.com>
2021-11-26 20:53:19 +0000

    added env for app url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 import { Options } from 'unique-selector';
 
-export const HF_URL = 'http://localhost:3000';
+export const HF_URL = process.env.HF_APP_URL || 'https://localhost:3000';
 export const pid = location.hostname.split('.')[0];
 export const uniqueSelectorOptions: Options = {
   selectorTypes: ['Tag', 'NthChild', 'ID', 'Attributes'],
